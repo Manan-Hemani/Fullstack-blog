e026e09527ebbae748377c9b5e598c3da1920efa
Manan-Hemani <mananhemani29@gmail.com>
2023-03-04 19:46:03 +0530

    added single page and styled it other pages

--- client/src/Pages/Single.jsx
+++ client/src/Pages/Single.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Edit from '../images/edit.png';
+import Delete from '../images/delete.png';
+import { Link } from 'react-router-dom';
+import Menu from '../Components/Menu';
 
 const Single = () => {
     return (
-    <div>
-        
+    <div className="single">
+        <div className="content">
+            <img src="" alt=""/>
+            <div className="user">
+                <img src="" alt=""/>
+                <div className="info">
+                    <span>John</span>
+                    <p>Posted 2 days ago</p>
+                </div>
+                <div className="edit">
+                    <Link to={`/write?edit=2`}>
+                        <img src={Edit} alt="edit"/>
+                    </Link>
+                    <img src={Delete} alt="delete"/>
+                </div>
+            </div>
+            <h1></h1>
+            <p></p>
+        </div>
+        <Menu />
     </div>
     )
 }
